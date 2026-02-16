6600fbc7a072ff4e7cf187fa52273716d89bd894
WangJiaLe <15768089+wangs-joyful-home@user.noreply.gitee.com>
2026-02-16 21:40:18 +0800

    修改文件结构

--- compile_server/compiler_test.cc
+++ compile_server/compiler_test.cc
@@ -1,27 +1,32 @@
-#include "compiler.hpp"
-#include "runner.hpp"
+#include "compile_run.hpp"
 
 int main()
 {
     init_logger();
-    Compiler cp;
-    std::string filename = "good";
-    if(cp.compile(filename))
+    int i = 4;
+    while(i--)
     {
-        std::cout << "compile success" << std::endl;
-    }
-    else
-    {
-        std::cout << "compile error" << std::endl;
-    }
-    Runner rn;
-    if(!rn.run(filename, 6, 1024))
-    {
-        std::cout << "run success" << std::endl;
-    }
-    else
-    {
-        std::cout << "run end with error" << std::endl;
+        std::cout << "******************test" << i << "******************" << std::endl;
+        std::string in_string;
+        std::string out_string;
+        Json::Value in_json;
+        if(i == 3)
+            in_json["code"] = "#include <iostream>\n int main()\n{ std::cout << 1 << std::endl;\n return 0;\n}";
+        else if(i == 2)
+            in_json["code"] = "#inclde <iostream>\n int main()\n{ std::cout << 1 << std::endl;\n return 0;\n}";
+        else if(i == 1) 
+            in_json["code"] = "#include <iostream>\n struct A\n{\nint a;\n};\nint main()\n{ A* pa = nullptr;\nstd::cout << pa->a << std::endl;\n return 0;\n}";
+        else if(i == 0)
+            in_json["code"] = "#include <iostream>\n int main()\n{\nstd::cerr << 1 << std::endl;\n return 0;\n}";
+
+        in_json["input"] = "null";
+        in_json["cpu_limit"] = 2;
+        in_json["mem_limit"] = 1024 * 8;
+        Json::StyledWriter writer;
+        in_string = writer.write(in_json);
+        
+        CompileAndRun::compile_and_run(in_string, out_string);
+        DEBUG("result: {}", out_string);
     }
     return 0;
 }
